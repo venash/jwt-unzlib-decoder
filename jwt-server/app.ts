c06df677bc48ae8940d185801f4535e3348201fb
Vaclav Rasendorfer <Rasendorfer.Vaclav-ext@komix.cz>
2020-08-25 21:34:45 +0200

    replacing unzlib with inflate, clean some stuff

--- jwt-server/app.ts
+++ jwt-server/app.ts
@@ -1,6 +1,27 @@
 import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 import { renderFileToString } from 'https://deno.land/x/dejs@0.8.0/mod.ts';
+import { parse } from "https://deno.land/std/flags/mod.ts";
+
 import { decodeTokenPayload, Token } from "./decoder.ts";
+
+
+const {args, exit} = Deno;
+const DEFAULT_PORT = 8000;
+const argPort = parse(args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
+if (isNaN(port)){
+    console.log("This is not port number");
+    exit(1);
+};
+console.log(port);
+/*
+const argPort = flags.parse(args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
+if (isNaN(port)){
+    console.log("This is not port number");
+    exit(1);
+};
+*/
 
 const app = new Application();
 
@@ -10,6 +31,8 @@ const router = new Router();
 Serve index.html
  */
 router.get('/', async (ctx) => {
+    console.log(parse(Deno.args));
+
     const body = await renderFileToString(Deno.cwd() + '/index.ejs', {});
     ctx.response.body = body;
 });
@@ -32,4 +55,4 @@ app.use(async (ctx) => {
     await send(ctx, ctx.request.url.pathname);
 })
 
-app.listen({ port: 8000 });
+app.listen({ port: port });
--- jwt-server/decoder.ts
+++ jwt-server/decoder.ts
@@ -1,4 +1,6 @@
-import { unzlib } from "https://deno.land/x/denoflate/mod.ts";
+import {
+    inflate,
+} from "https://deno.land/x/compress@v0.3.3/mod.ts";
 
 export interface Token {
     token: string;
@@ -8,7 +10,7 @@ export function decodeTokenPayload(token: string): string {
     const payloadSubstring = token.split('.')[1];
     const base64 = payloadSubstring.replace(/-/g, '+').replace(/_/g, '/');
     const bytes = Uint8Array.from(atob(base64), c => c.charCodeAt(0));
-    const uzlibed = unzlib(bytes)
+    const uzlibed = inflate(bytes)
     const decoded = new TextDecoder().decode(uzlibed);
     return decoded;
 }
